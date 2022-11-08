bbe51e5a23aa3278420c00da6f73b0e80e4b2a4b
Jaroslav Holeček <holecj14@fel.cvut.cz>
2022-11-08 10:10:55 +0100

    Nové složky + úprava 2019_IT - JOIN

--- 2019_IT/drawSQL-mysql-export-2022-09-20.sql
+++ 2019_IT/drawSQL-mysql-export-2022-09-20.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Osoba`(
     `BirthDate` DATE NOT NULL,
     `IDnumber` INT(3) NOT NULL,
     `Firstname` CHAR(20) NOT NULL,
-    `id_TrainingGroup` INT(3) UNSIGNED NULL
+    `id_TrainingGroup` INT(3) UNSIGNED
 );
 
 CREATE TABLE `TrainingGroup`(
@@ -19,16 +19,17 @@ ALTER TABLE
 
 /* Vkládání dat */
 INSERT INTO TrainingGroup (Name) VALUES
-("Pondělí"), ("Úterý"), ("Středa");
+("Pondělí"), ("Úterý"), ("Středa"), ("Čtvrtek"), ("Pátek");
 
 INSERT INTO
-Osoba (BirthDate, IDnumber, Firstname, id_TrainingGroup)
-VALUES
+Osoba (BirthDate, IDnumber, Firstname, id_TrainingGroup) VALUES
 ("2020-10-4", 68, "Jarda", (SELECT id FROM `TrainingGroup` WHERE Name="Pondělí")),
 ("2000-12-5", 71, "Franta", (SELECT id FROM `TrainingGroup` WHERE Name="Pondělí")),
 ("2020-10-4", 80, "Bára", (SELECT id FROM `TrainingGroup` WHERE Name="Úterý")),
 ("2020-10-4", 35, "Klára", (SELECT id FROM `TrainingGroup` WHERE Name="Středa")),
 ("1999-1-1", 38, "Adéla", (SELECT id FROM `TrainingGroup` WHERE Name="Středa"));
+INSERT INTO Osoba(BirthDate, IDnumber, Firstname) VALUES
+("1999-12-24", 50, "Eliška");
 
 
 /* Čtení dat */
@@ -42,5 +43,7 @@ SELECT * FROM Osoba WHERE id_TrainingGroup = (SELECT id FROM `TrainingGroup` WHE
                             AND BirthDate < "2015-1-1" /* je "starý" */
 
 /* Select s JOIN */
-SELECT Firstname, TrainingGroup.Name FROM `Osoba` JOIN TrainingGroup WHERE id_TrainingGroup = TrainingGroup.id;
+SELECT Firstname, TrainingGroup.Name FROM `Osoba` JOIN TrainingGroup ON id_TrainingGroup = TrainingGroup.id;
+SELECT Firstname, TrainingGroup.Name FROM `Osoba` LEFT JOIN TrainingGroup ON id_TrainingGroup = TrainingGroup.id;
+/* MariaDB neumí FULL OUTER -> je potřeba ho "nasimulovat" https://stackoverflow.com/questions/4796872/how-can-i-do-a-full-outer-join-in-mysql */
 
