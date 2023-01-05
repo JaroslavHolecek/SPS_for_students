5430740416b1d324d7d6aec6483da8c63b1279ab
Jaroslav Holeček <holecj14@fel.cvut.cz>
2023-01-05 09:59:20 +0100

    pyqt layouts

--- 2020_IT/Algoritmy/pyqt.py
+++ 2020_IT/Algoritmy/pyqt.py
@@ -10,17 +10,28 @@ main.setWindowTitle('Hello Qt')
 layout = QtWidgets.QHBoxLayout()
 main.setLayout(layout)
 
+layout_vertical = QtWidgets.QVBoxLayout()
+
+
 # Nápis
 label = QtWidgets.QLabel('Click the button to change me')
 # Přidáním do layoutu se nápis automaticky stane potomkem hlavního okna
 layout.addWidget(label)
 
+layout.addLayout(layout_vertical)
+
 # Tlačítko
 button = QtWidgets.QPushButton('Click me')
-layout.addWidget(button)
+layout_vertical.addWidget(button)
+
+buttonDva = QtWidgets.QPushButton("Cudlik 2")
+layout_vertical.addWidget(buttonDva)
 
 vstup = QtWidgets.QLineEdit()
 layout.addWidget(vstup)
+
+vstupDva = QtWidgets.QLineEdit()
+layout.addWidget(vstupDva)
 # vstup.text()
 # Funkcionalita
 
@@ -38,9 +49,24 @@ def change_label():
     z = sum(map(int, vstup.text().split(" ")))
     
     label.setText(f"{y} je stejné jako {z}")
+
+
+def change_labelDva():
+    celytext = vstupDva.text()
+    print(celytext)
+    rozdelenepole = celytext.split(" ")
+    print(rozdelenepole)
+    y = 0
+    for i in rozdelenepole:
+        y += int(i)
     
+    z = sum(map(int, vstupDva.text().split(" ")))
+    
+    label.setText(f"{y} je stejné jako {z}")
 
 button.clicked.connect(change_label)
+
+buttonDva.clicked.connect(change_labelDva)
 
 # Spuštění
 main.show()
