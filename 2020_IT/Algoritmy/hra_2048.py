24393338b8c4d2684f123e2716f16624cd6dd969
Jaroslav Holeček <holecek.jaroslav@emial.cz>
2023-02-27 14:00:26 +0100

    event, blit

--- 2020_IT/Algoritmy/hra_2048.py
+++ 2020_IT/Algoritmy/hra_2048.py
@@ -18,8 +18,14 @@ hlavni_platno.blit(mraky, (0, 0))
 pozice_kolecka = [25,40]
 pygame.draw.circle(hlavni_platno, BLUE, pozice_kolecka, 20)
 
+#vectorize pojmenovani vlastni udalosti
+EVENT_POSUN_KOLECKA = pygame.USEREVENT + 1
+
 hodiny = pygame.time.Clock()
 FPS = 60
+
+#udalost se vyvola kazdych 2000 milisekund
+pygame.time.set_timer(EVENT_POSUN_KOLECKA, 2000)
 
 bezi = True
 while bezi: # hlavní smyčka
@@ -28,9 +34,22 @@ while bezi: # hlavní smyčka
             bezi = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
-                pozice_kolecka[0] += 5
+                # vyvolani udalosti pomoci prikazu
+                pygame.event.post(pygame.event.Event(EVENT_POSUN_KOLECKA))
 
-    hlavni_platno.blit(mraky, (0, 0))
+        if event.type == EVENT_POSUN_KOLECKA:
+            pozice_kolecka[0] += 5
+
+    # okamzite zpracovani klavesnice - bez udalosti
+    klavesy = pygame.key.get_pressed()
+    if klavesy[pygame.K_LEFT]:
+        pozice_kolecka[0] += 5
+
+
+    # kam.blit(odkud, dest=pozice_kam, area=velikost)
+    hlavni_platno.blit(mraky, dest=(0, 0))
+
+    
     pygame.draw.circle(hlavni_platno, BLUE, pozice_kolecka, 20)
     pygame.display.update() # vykreslení změn na monitor
     hodiny.tick(FPS)
