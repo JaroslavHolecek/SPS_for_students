a49364c5d5f916bb0cf3238217f3dfc73fca2713
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-02-28 13:30:34 +0100

    zpracovani vstupu

--- 2019_IT/Maturita/Snake.py
+++ 2019_IT/Maturita/Snake.py
@@ -1,22 +1,26 @@
 import pygame
 pygame.init()
 
-
 background_colour = (255,255,255)
-(width, height) = (300, 200)
+RED = pygame.Color("red")
 
+(width, height) = (300, 200)
 screen = pygame.display.set_mode((width, height))
 
+pygame.display.set_caption('Snake')
 
-
-pygame.display.set_caption('Tutorial 1')
 screen.fill(background_colour)
+
 pygame.display.flip()
+
 running = True
 while running:
-  for event in pygame.event.get():
+  # Zpracování vstupu
+  for event in pygame.event.get(): # fronta událostí
     if event.type == pygame.QUIT:
       running = False
+
+  stav_klaves = pygame.key.get_pressed() # okamžitý stav kláves
 
 
 pygame.quit()
