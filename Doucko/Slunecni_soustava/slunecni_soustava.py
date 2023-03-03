c23900d101171c3291b79cda29a9e2c19fe87a4c
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-03-03 11:51:59 +0100

    prex

--- Doucko/Slunecni_soustava/slunecni_soustava.py
+++ Doucko/Slunecni_soustava/slunecni_soustava.py
@@ -1,3 +1,4 @@
+import math
 # import the pygame module
 import pygame
 pygame.init()
@@ -20,16 +21,19 @@ pygame.display.set_caption('Sluneční soustava')
 # Fill the background colour to the screen
 screen.fill(background_colour)
 
-
+pozice_slunce = [SCREEN_SIZE[0]//2, SCREEN_SIZE[1]//2]
 polomer_slunce = 20
-
+            #  0    1
 pozice_zeme = [100, 100]
 polomer_zeme = 5
+vzdalenost_zeme_od_slunce = 100
+rychlost_obehu = 0.001
 
 
 hodiny = pygame.time.Clock()
-FPS = 50
+FPS = 100
 
+start_time = pygame.time.get_ticks()
 running = True
 # game loop
 while running:
@@ -39,10 +43,16 @@ while running:
             running = False
 
     # VYPOCTY
+    actual_time = pygame.time.get_ticks()
+    ubehnuty_cas = actual_time - start_time
 
+    pozice_zeme[0] = int(pozice_slunce[0] + vzdalenost_zeme_od_slunce*math.sin(rychlost_obehu * ubehnuty_cas) ) # x
+    pozice_zeme[1] = int(pozice_slunce[1] + vzdalenost_zeme_od_slunce*math.cos(rychlost_obehu * ubehnuty_cas) ) # y
 
     # VYKRESLENI
-    pygame.draw.circle(screen, RED, [SCREEN_SIZE[0]//2, SCREEN_SIZE[1]//2], polomer_slunce )
+    screen.fill(background_colour)
+
+    pygame.draw.circle(screen, RED, pozice_slunce, polomer_slunce )
     pygame.draw.circle(screen, BLUE, pozice_zeme, polomer_zeme)
 
     pygame.display.flip()
