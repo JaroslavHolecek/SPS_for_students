c02dd52b31b11e58d8888206b5dc3ff4c05b5dbc
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-03-06 13:41:44 +0100

    kostky jako sprite, naraz do praveho okraje

--- 2020_IT/Algoritmy/hra_2048.py
+++ 2020_IT/Algoritmy/hra_2048.py
@@ -15,17 +15,40 @@ pygame.display.set_caption('Hra 2048')
 mraky = pygame.transform.scale(mraky, (HLAVNI_OKNO_SIRKA, HLAVNI_OKNO_VYSKA))
 hlavni_platno.blit(mraky, (0, 0))
 
-pozice_kolecka = [25,40]
-pygame.draw.circle(hlavni_platno, BLUE, pozice_kolecka, 20)
+
+class Kostka(pygame.sprite.Sprite):
+    def __init__(self,X,Y):
+        super().__init__()
+
+        self.image = pygame.Surface([25, 25])
+        self.image.fill(BLUE)
+
+        self.rect = self.image.get_rect()
+
+        self.rect[0] = X
+        self.rect[1] = Y
+
+    def update(self ):
+        self.rect[0] += 5
+        if self.rect[0] + self.rect[2] > HLAVNI_OKNO_SIRKA:
+            self.rect[0] = HLAVNI_OKNO_SIRKA - self.rect[2]
+
+skupina_kostek = pygame.sprite.Group()
+
+for k in range(10):
+    skupina_kostek.add(Kostka(k*30, k*30))
+
 
 #vectorize pojmenovani vlastni udalosti
-EVENT_POSUN_KOLECKA = pygame.USEREVENT + 1
+EVENT_POSUN_KOSTKY = pygame.USEREVENT + 1
 
 hodiny = pygame.time.Clock()
 FPS = 60
 
 #udalost se vyvola kazdych 2000 milisekund
-pygame.time.set_timer(EVENT_POSUN_KOLECKA, 2000)
+pygame.time.set_timer(EVENT_POSUN_KOSTKY, 1000)
+
+
 
 bezi = True
 while bezi: # hlavní smyčka
@@ -35,22 +58,24 @@ while bezi: # hlavní smyčka
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
                 # vyvolani udalosti pomoci prikazu
-                pygame.event.post(pygame.event.Event(EVENT_POSUN_KOLECKA))
+                pygame.event.post(pygame.event.Event(EVENT_POSUN_KOSTKY))
 
-        if event.type == EVENT_POSUN_KOLECKA:
-            pozice_kolecka[0] += 5
+        if event.type == EVENT_POSUN_KOSTKY:
+            skupina_kostek.update()
 
     # okamzite zpracovani klavesnice - bez udalosti
-    klavesy = pygame.key.get_pressed()
-    if klavesy[pygame.K_LEFT]:
-        pozice_kolecka[0] += 5
+    # klavesy = pygame.key.get_pressed()
+    # if klavesy[pygame.K_LEFT]:
+    #     pozice_kolecka[0] += 5
 
 
     # kam.blit(odkud, dest=pozice_kam, area=velikost)
     hlavni_platno.blit(mraky, dest=(0, 0))
 
+    skupina_kostek.draw(hlavni_platno)
 
-    pygame.draw.circle(hlavni_platno, BLUE, pozice_kolecka, 20)
+
+    # pygame.draw.circle(hlavni_platno, BLUE, pozice_kolecka, 20)
     pygame.display.update() # vykreslení změn na monitor
     hodiny.tick(FPS)
 
