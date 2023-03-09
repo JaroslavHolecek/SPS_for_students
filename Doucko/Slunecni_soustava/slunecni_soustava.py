25abb4b6c6a56e3b4e262dc45a736edb62cd558f
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-03-09 15:18:30 +0100

    planety jako sprite

--- Doucko/Slunecni_soustava/slunecni_soustava.py
+++ Doucko/Slunecni_soustava/slunecni_soustava.py
@@ -23,11 +23,32 @@ screen.fill(background_colour)
 
 pozice_slunce = [SCREEN_SIZE[0]//2, SCREEN_SIZE[1]//2]
 polomer_slunce = 20
-            #  0    1
-pozice_zeme = [100, 100]
-polomer_zeme = 5
-vzdalenost_zeme_od_slunce = 100
-rychlost_obehu = 0.001
+
+class Planeta(pygame.sprite.Sprite):
+    def __init__(self, polomer, vzdalenost_od_slunce, rychlost_obehu): # initialization - vytvoření
+        pygame.sprite.Sprite.__init__(self) #super.__init__(self)
+
+        self.image = pygame.Surface([2*polomer, 2*polomer])
+        pygame.draw.circle(self.image, BLUE, [polomer, polomer], polomer)
+
+        # rectangle -> [levy_horni_x, levy_horni_y, sirka, vyska]
+        self.rect = self.image.get_rect()
+
+        self.vzdalenost_od_slunce = vzdalenost_od_slunce
+        self.rychlost_obehu = rychlost_obehu
+
+    def update(self, ubehnuty_cas):
+        self.rect.x = int(
+            pozice_slunce[0] + self.vzdalenost_od_slunce * math.sin(self.rychlost_obehu * ubehnuty_cas))  # x
+        self.rect.y = int(
+            pozice_slunce[1] + self.vzdalenost_od_slunce * math.cos(self.rychlost_obehu * ubehnuty_cas))  # y
+
+
+vsechny_planety = pygame.sprite.Group()
+vsechny_planety.add(Planeta(polomer=5, vzdalenost_od_slunce=100, rychlost_obehu=0.001))
+vsechny_planety.add(Planeta(2, 50, 0.002))
+# for i in range(1,10):
+#     vsechny_planety.add(Planeta(5, 100, i*0.001))
 
 
 hodiny = pygame.time.Clock()
@@ -46,14 +67,13 @@ while running:
     actual_time = pygame.time.get_ticks()
     ubehnuty_cas = actual_time - start_time
 
-    pozice_zeme[0] = int(pozice_slunce[0] + vzdalenost_zeme_od_slunce*math.sin(rychlost_obehu * ubehnuty_cas) ) # x
-    pozice_zeme[1] = int(pozice_slunce[1] + vzdalenost_zeme_od_slunce*math.cos(rychlost_obehu * ubehnuty_cas) ) # y
+    vsechny_planety.update(ubehnuty_cas)
 
     # VYKRESLENI
     screen.fill(background_colour)
 
-    pygame.draw.circle(screen, RED, pozice_slunce, polomer_slunce )
-    pygame.draw.circle(screen, BLUE, pozice_zeme, polomer_zeme)
+    vsechny_planety.draw(screen)
+    pygame.draw.circle(screen, RED, pozice_slunce, polomer_slunce)
 
     pygame.display.flip()
     hodiny.tick(FPS)
