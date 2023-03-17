affbc1112ae85a8eaa956066bbd6b9d1a8daad0b
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-03-17 13:17:54 +0100

    pauza hry

--- 2019_EP/Maturita/space_invader.py
+++ 2019_EP/Maturita/space_invader.py
@@ -8,6 +8,10 @@ background_colour = (234, 212, 252)
 background = pygame.Surface([sirka, vyska])
 background.fill(background_colour)
 
+pygame.font.init()
+
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 black = (0, 0, 0)
 screen = pygame.display.set_mode((sirka, vyska))
 
@@ -29,12 +33,9 @@ class Hrac(pygame.sprite.Sprite):
             self.rect[0] = self.rect[0] - self.rychlost * (dt//10)
         if klavesy[pygame.K_d]:
             self.rect[0] = self.rect[0] + self.rychlost * (dt//10)
-        if klavesy[pygame.K_NUMLOCK]:
+        if klavesy[pygame.K_SPACE]:
             if self.maxpocetstrel > len(seznamstrel):
                 seznamstrel.add(Strela(self))
-
-
-
 
 
 class Nepritel(pygame.sprite.Sprite):
@@ -65,10 +66,12 @@ class Strela(pygame.sprite.Sprite):
 vsichni_hraci = pygame.sprite.Group()
 
 vsichni_hraci.add(Hrac())
+def pridej_nepratele(pocet, kam):
+    for p in range(pocet):
+        kam.add(Nepritel(random.randint(0, sirka), 10))
 
 vsichni_nepratele = pygame.sprite.Group()
-for p in range(10):
-    vsichni_nepratele.add(Nepritel(random.randint(0,sirka),10))
+pridej_nepratele(2,vsichni_nepratele)
 
 vsechny_strely = pygame.sprite.Group()
 
@@ -76,30 +79,54 @@ vsechny_strely = pygame.sprite.Group()
 pygame.display.flip()
 clock = pygame.time.Clock()
 FPS = 60
-
 dt = clock.tick(FPS)
 running = True
+PAUSE = 1
+stav_hry = PAUSE
+BEZI = 0
+unpaused = False
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                if stav_hry == BEZI:
+                    stav_hry = PAUSE
+                elif stav_hry == PAUSE:
+                    stav_hry = BEZI
+                    unpaused = True
 
-    klavesy = pygame.key.get_pressed()
-    vsichni_hraci.update(klavesy,vsechny_strely)
+    if PAUSE == stav_hry:
+        pass
 
-        #výpočty
-    vsechny_strely.update(dt)
-    vsichni_nepratele.update(dt)
+    elif stav_hry == BEZI:
+        # výpočty
+        klavesy = pygame.key.get_pressed()
+        vsichni_hraci.update(klavesy, vsechny_strely)
+        vsechny_strely.update(dt)
+        vsichni_nepratele.update(dt)
 
-    pygame.sprite.groupcollide(vsechny_strely, vsichni_nepratele, True, True)
 
-    for strela in vsechny_strely:
-        if strela.rect.y < 0:
-            vsechny_strely.remove(strela)
+        pygame.sprite.groupcollide(vsechny_strely, vsichni_nepratele, True, True)
+        if len(vsichni_nepratele)< 2:
+            pridej_nepratele(3, vsichni_nepratele)
+
+        for strela in vsechny_strely:
+            if strela.rect.y < 0:
+                vsechny_strely.remove(strela)
 
     #vykreslovani
     # screen.blit(obrazek_hrac,dest = hrac_pozice)
     # screen.blit(obrazek_enemy, dest=enemy_pozice)
+    if stav_hry == PAUSE:
+        text_surface = my_font.render('Pause: Press Escape to Continue', False, (0, 0, 0))
+        screen.blit(text_surface, (sirka//2,vyska//2))
+    if unpaused == True:
+        screen.fill(background_colour)
+        unpaused = False
+
     vsichni_hraci.clear(screen, background)
     vsichni_hraci.draw(screen)
     vsechny_strely.clear(screen, background)
