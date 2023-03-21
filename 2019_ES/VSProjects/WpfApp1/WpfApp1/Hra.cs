f80fcab6ca0ad23a4998f7796d5f30ba2bd2f0e0
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-03-21 12:32:33 +0100

    prezentace

--- 2019_ES/VSProjects/WpfApp1/WpfApp1/Hra.cs
+++ 2019_ES/VSProjects/WpfApp1/WpfApp1/Hra.cs
@@ -19,10 +19,6 @@ namespace SpaceInvader
         public Hrac hrac;
         public List<Strela> strely = new List<Strela>();
         public List<Invader> seznam_invader = new List<Invader>();
-
-        /*public Zeton z1;
-        public List<Zeton> vsechny_zetony = new List<Zeton>();
-        public List<Prekazka> vsechny_prekazky = new List<Prekazka>();*/
         MainWindow mw;
 
         public int GRAVITACE;
@@ -38,41 +34,10 @@ namespace SpaceInvader
 
             /*Random rnd = new Random();
             rnd.Next();
-            for (int i = 1; i < 11; i++)
-            {
-                vsichni_nepratele.Add(new Nepritel(8, 5 * i, 15 * i));
-            }
-
-            for (int i = 1; i < 11; i++)
-            {
-
-                pridej_zeton(new Zeton(i, 10 * i, 20 * i));
-            }
-
-            for (int i = 1; i < 11; i++)
-            {
-                vsechny_prekazky.Add(new Prekazka(50, 100 * i, 50 * i));
-            }
-
-            GRAVITACE = 2;*/
+            */
 
             platno.Children.Add(hrac.obrazek);
-            
-
-
-            /* TODO: přidat obrázky Invaderů do plátna */
-
-            /*foreach (Nepritel nepritel in vsichni_nepratele)
-            {
-                platno.Children.Add(nepritel.obrazek);
-            }
-
-
-            foreach (Prekazka p in vsechny_prekazky)
-            {
-                platno.Children.Add(p.obrazek);
-            }
-            */
+            pridejinvadery(5);
 
             InitializeHlavniTimer();
 
@@ -91,6 +56,11 @@ namespace SpaceInvader
             double strela_y = strela.pozice_Strely.Y;
             double strela_konec_y = strela.pozice_Strely.Y + strela.obrazek.Height;
 
+            if (((strela_x > invader_x && strela_x < invader_konec_x) || (strela_konec_x > invader_x && strela_konec_x < invader_konec_x))
+                && ((strela_y > invader_y && strela_y < invader_konec_y) || (strela_konec_y > invader_y && strela_konec_y < invader_konec_y)))
+            {
+                return true;
+            }
             return false;
         }
 
@@ -98,68 +68,11 @@ namespace SpaceInvader
         {
             for (int i = 0; i < pocet; i++)
             {
-                pridejinvadera();
+                pridejinvadera(new Point(60*i ,30 ));
             } 
         }
-        /*public bool naraz_hrace_do_nepritele(Hrac h, Nepritel n)
-        {
-            int polomer_hrace = (int)Math.Ceiling(h.obrazek.Width / 2);
-            int polomer_nepritele = (int)Math.Ceiling(n.obrazek.Width / 2);
-
-            Point stred_hrace = new Point(h.point.X + polomer_hrace, h.point.Y + polomer_hrace);
-            Point stred_nepritele = new Point(n.point.X + polomer_nepritele, n.point.Y + polomer_nepritele);
-
-            if ((stred_hrace.X - stred_nepritele.X) * (stred_hrace.X - stred_nepritele.X)
-                + (stred_hrace.Y - stred_nepritele.Y) * (stred_hrace.Y - stred_nepritele.Y)
-
-                < (polomer_hrace + polomer_nepritele) * (polomer_hrace + polomer_nepritele)
-                )
-            {
-                return true;
-
-            }
-            return false;
-        }
 
 
-        public bool naraz_hrace_do_prekazky(Hrac h, Prekazka n)
-        {
-            int polomer_hrace = (int)Math.Ceiling(h.obrazek.Width / 2);
-            int polomer_prekazky = (int)Math.Ceiling(n.obrazek.Width / 2);
-
-            Point stred_hrace = new Point(h.point.X + polomer_hrace, h.point.Y + polomer_hrace);
-            Point stred_prekazky = new Point(n.point.X + polomer_prekazky, n.point.Y + polomer_prekazky);
-
-            if ((stred_hrace.X - stred_prekazky.X) * (stred_hrace.X - stred_prekazky.X)
-                + (stred_hrace.Y - stred_prekazky.Y) * (stred_hrace.Y - stred_prekazky.Y)
-
-                < (polomer_hrace + polomer_prekazky) * (polomer_hrace + polomer_prekazky)
-                )
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public bool naraz_hrace_do_zetonu(Hrac h, Zeton z)
-        {
-            int polomer_hrace = (int)Math.Ceiling(h.obrazek.Width / 2);
-            int polomer_prekazky = (int)Math.Ceiling(z.obrazek.Width / 2);
-
-            Point stred_hrace = new Point(h.point.X + polomer_hrace, h.point.Y + polomer_hrace);
-            Point stred_prekazky = new Point(z.point.X + polomer_prekazky, z.point.Y + polomer_prekazky);
-
-            if ((stred_hrace.X - stred_prekazky.X) * (stred_hrace.X - stred_prekazky.X)
-                + (stred_hrace.Y - stred_prekazky.Y) * (stred_hrace.Y - stred_prekazky.Y)
-
-                < (polomer_hrace + polomer_prekazky) * (polomer_hrace + polomer_prekazky)
-                )
-            {
-                return true;
-            }
-            return false;
-        }
-        */
         private void InitializeHlavniTimer()
         {
             this.hlavniTimer = new DispatcherTimer(DispatcherPriority.Normal); // For lagging Fix is used priority
@@ -168,19 +81,13 @@ namespace SpaceInvader
 
             this.hlavniTimer.Start();
         }
-        /*
-        public void smaz_zeton(Zeton z)
+
+        public void smaz_invadera(Invader i)
         {
-            vsechny_zetony.Remove(z);
-            platno.Children.Remove(z.obrazek);
+            seznam_invader.Remove(i);
+            platno.Children.Remove(i.obrazek);
         }
 
-        public void pridej_zeton(Zeton z)
-        {
-            vsechny_zetony.Add(z);
-            platno.Children.Add(z.obrazek);
-        }
-        */
         public void smaz_strelu(Strela j)
         {
             strely.Remove(j);
@@ -200,9 +107,9 @@ namespace SpaceInvader
             }
         }
 
-        public void pridejinvadera()
+        public void pridejinvadera(Point p)
         {
-            seznam_invader.Add(new Invader(new Point(25, 20))); /* TODO: invadery přidávat na náhodné pozice  */
+            seznam_invader.Add(new Invader(p)); /* TODO: invadery přidávat na náhodné pozice  */
             platno.Children.Add(seznam_invader[seznam_invader.Count - 1].obrazek);
         }
 
@@ -217,6 +124,11 @@ namespace SpaceInvader
             /* ======================== */
             /* ==== výpočty ve hře ==== */
             /* ======================== */
+            foreach (Invader i in seznam_invader)
+            {
+                i.pohyb(platno);
+            }
+                bool break_strely = false;
             foreach (Strela s in strely)
             {
                 s.pohyb(platno);
@@ -225,48 +137,25 @@ namespace SpaceInvader
                     smaz_strelu(s);
                     break;
                 }
+
+                break_strely = false;
+                foreach (Invader i in seznam_invader)
+                {
+                    if (kolize_strely_invader(i,s))
+                    {
+                        smaz_strelu(s);
+                        smaz_invadera(i);
+                        break_strely = true;
+                        break;
+                    }
+                }
+                if (break_strely)
+                {
+                    break;
+                }
             }
-            
-            /* pohyb nepratel */
-            /*foreach (Nepritel n in vsichni_nepratele)
-            {
-                n.pohyb();
-            }
 
-            */
-            /* gravitace */
-            /*hrac.nastavPozici(platno, new Point(hrac.point.X, hrac.point.Y + GRAVITACE));*/
-
-
-            /* foreach (Nepritel n in vsichni_nepratele)
-             {
-                 if (naraz_hrace_do_nepritele(hrac, n))
-                 {
-                     hrac.body -= 1;
-                     hrac.nastavPozici(platno, new Point(150, 150));
-                 }
-             }
-
-             foreach (Prekazka p in vsechny_prekazky)
-             {
-                 if (naraz_hrace_do_prekazky(hrac, p))
-                 {
-                     hrac.nastavPozici(platno, hrac.point_predchozi);
-                     hrac.stojim_na_zemi = true;
-                 }
-             }
-
-
-             foreach (Zeton zeton in vsechny_zetony)
-             {
-                 if (naraz_hrace_do_zetonu(hrac, zeton))
-                 {
-                     hrac.body += zeton.hodnota;
-                     smaz_zeton(zeton);
-                     break;
-                 }
-             }
-             */
+           
             /* ==================== */
             /* ==== Vykresleni ==== */
             /* ==================== */
