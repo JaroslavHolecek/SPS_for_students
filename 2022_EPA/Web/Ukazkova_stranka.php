37d23296f570fa3cf0cef4bd9fa9f0536e6155cc
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-09-26 09:38:21 +0200

    EPA cykly

--- 2022_EPA/Web/Ukazkova_stranka.php
+++ 2022_EPA/Web/Ukazkova_stranka.php
@@ -3,8 +3,38 @@
 <body>
 
 <?php
-$VelikostNadpisu = 2;
+ 
+$x = 0;
 echo "My first PHP script!";
+/*datove typy -
+int - cele cislo
+float - cislo  s desetinnou carkou
+string - text
+boolean - true/false
+Jak zjistit datovy typ 
+----------------------*/
+$VelikostNadpisu= 0;
+var_dump($VelikostNadpisu); 
+
+
+while ($VelikostNadpisu <= 10){
+	$VelikostNadpisu = $VelikostNadpisu+1;
+	if ($VelikostNadpisu %6 == 0) {
+		break;	# code...
+	}
+	if ($VelikostNadpisu %2 == 0) {
+		continue;	# code...
+	}
+	
+
+	echo ("ghghgg $VelikostNadpisu<br>");
+	
+	
+}
+
+for ($i=0; $i < 5; $i+=2 ){ 
+	echo "$i";
+}
 ?>
 
 <h1>nadpis1</h1>
