f8abc0346328b6fad32ffc40ea70973e40bc7666
Jaroslav Holeček <holecek.jaroslav@email.cz>
2025-01-07 13:06:45 +0100

    projekty

--- 2023_IT/Had/main.py
+++ 2023_IT/Had/main.py
@@ -9,7 +9,8 @@ clock = pygame.time.Clock()
 running = True # boolean
 dt = 0 # int
 FPS = 60
-pozadi = pygame.Color("purple")
+font_color = pygame.Color("purple")
+font_info = pygame.font.Font(None, 48)
 background_image = pygame.image.load("images/pozadi_vlk.jpg")  # Nahraďte 'background.jpg' cestou k vašemu obrázku
 background_image = pygame.transform.scale(background_image, screen.get_size())  # Přizpůsobení velikosti obrazovky
 
@@ -17,13 +18,19 @@ background_image = pygame.transform.scale(background_image, screen.get_size())  
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 player_radius = 40
 player_color = pygame.Color("red")
-player_speed = 300
+player_speed = 100
 
 MOVE_UP = pygame.Vector2(0, -1)
 MOVE_DOWN = pygame.Vector2(0, 1)
 MOVE_LEFT = pygame.Vector2(-1, 0)
 MOVE_RIGHT = pygame.Vector2(1, 0)
+MOVE_NONE = pygame.Vector2(0, 0)
+automatic_move = MOVE_NONE.copy()
 
+STATE_PLAY = 0
+STATE_PAUSE = 1
+
+ACTUAL_STATE = STATE_PLAY
 # >>>>>>>> hlavni cyklus hry <<<<<<<<<
 while running:
     # poll for events
@@ -32,21 +39,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if ACTUAL_STATE == STATE_PLAY:
+                    ACTUAL_STATE = STATE_PAUSE
+                elif ACTUAL_STATE == STATE_PAUSE:
+                    ACTUAL_STATE = STATE_PLAY
 
-    keys = pygame.key.get_pressed()
+    if ACTUAL_STATE == STATE_PLAY:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            automatic_move = MOVE_UP
+        if keys[pygame.K_s]:
+            automatic_move = MOVE_DOWN
+        if keys[pygame.K_a]:
+            automatic_move = MOVE_LEFT
+        if keys[pygame.K_d]:
+            automatic_move = MOVE_RIGHT
 
-    player_move = pygame.Vector2(0,0)
-    if keys[pygame.K_w]:
-        player_move += MOVE_UP
-    if keys[pygame.K_s]:
-        player_move += MOVE_DOWN
-    if keys[pygame.K_a]:
-        player_move += MOVE_LEFT
-    if keys[pygame.K_d]:
-        player_move += MOVE_RIGHT
+
 
     # >>>>>>>> výpočty hry <<<<<<<<<
-    player_pos += player_move * player_speed * dt
+    if ACTUAL_STATE == STATE_PLAY:
+        player_move = MOVE_NONE.copy()
+        player_move += automatic_move
+        player_pos += player_move * player_speed * dt
+
+
 
 
     # fill the screen with a color to wipe away anything from last frame
@@ -54,6 +73,13 @@ while running:
     screen.blit(background_image, (0, 0))
 
     pygame.draw.circle(screen, player_color, player_pos, player_radius)
+
+    if ACTUAL_STATE == STATE_PAUSE:
+        # Nápis PAUZA
+        text_pauza = "PAUZA - mezerník pro spuštění"
+        text_picture = font_info.render(text_pauza, True, font_color)
+        screen.blit(text_picture, (screen.get_width() / 2, screen.get_height() / 2))
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
