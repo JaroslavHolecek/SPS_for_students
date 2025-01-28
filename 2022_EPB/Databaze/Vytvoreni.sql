1e256b4c6f7b54c583a978a1756bb21cf526922e
Jaroslav Holeček <holecek.jaroslav@email.cz>
2025-01-28 14:37:21 +0100

    insert

--- 2022_EPB/Databaze/Vytvoreni.sql
+++ 2022_EPB/Databaze/Vytvoreni.sql
@@ -3,6 +3,7 @@ CREATE TABLE 2022_EPB_Uzivatel (
     email char(50),
     heslo char(50)
 );
+INSERT INTO 2022_EPB_Uzivatel (email, heslo) VALUES ("ahoj@mail.cz", "ahoj123"), ("cau@mail.cz", "cau123"); 
 
 CREATE TABLE 2022_EPB_Postava (
 	id int AUTO_INCREMENT PRIMARY KEY,
@@ -11,3 +12,7 @@ CREATE TABLE 2022_EPB_Postava (
     uzivatel_id int NOT NULL,
     FOREIGN KEY (uzivatel_id) REFERENCES 2022_EPB_Uzivatel(id)
 );
+INSERT INTO 2022_EPB_Postava(jmeno, hp, uzivatel_id) VALUES ("Adam", 1000, 1);
+
+SELECT id FROM `2022_EPB_Uzivatel` WHERE email="ahoj@mail.cz"; 
+INSERT INTO 2022_EPB_Postava(jmeno, hp, uzivatel_id) VALUES ("Bára", 1500, (SELECT id FROM `2022_EPB_Uzivatel` WHERE email="cau@mail.cz") ); 
