2aa8081016d8d4f7c2830cb687a4b3afda695af2
Jaroslav Holeček <holecek.jaroslav@email.cz>
2026-02-17 11:20:46 +0100

    pygame

--- 2024_EP/Python/Pygame/zkusebna.py
+++ 2024_EP/Python/Pygame/zkusebna.py
@@ -3,6 +3,23 @@ import pygame
 
 # pygame setup
 pygame.init()
+
+class Hrac(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+       pygame.sprite.Sprite.__init__(self)
+
+       self.image = pygame.image.load("./2024_EP/Python/Pygame/lod.png")
+
+       self.rect = self.image.get_rect()
+
+
+hrac1 = Hrac("red", 200, 50)
+
+vsichni_hraci = pygame.sprite.Group()
+vsichni_hraci.add(hrac1)
+
+
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
@@ -10,17 +27,13 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
+# Hlavní smyčka
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+
+    ## Zpracování vstupu
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -32,12 +45,20 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    ## Výpočty ve hře
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    ## Zobrazení stavu na monitor
+    screen.fill("purple") # smazat starý stav
+
+    # vykreslit nový stav
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    vsichni_hraci.draw(screen)
+
+    pygame.display.flip() # odeslání změn na monitor
+
+    ## Pauza pro FPS
+    dt = clock.tick(60) / 1000 # pauza + kolik času uběhlo v sekundách
+
+    
 
 pygame.quit()
